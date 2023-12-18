c4de41a4bfabe7576f96d1fe98943f2e8ff02e31
Mochamad Nahrul <mochnahrul.wd@gmail.com>
2023-12-18 10:41:30 +0700

    change the output response

--- app/resources/course.py
+++ app/resources/course.py
@@ -5,26 +5,26 @@ from flask_restx import Resource, Namespace
 from .. import api, db
 from ..models import Course, Student
 from ..api_models import course_response_model, course_model
+from ..utils import generate_response
 
 
 course_ns = Namespace("Course", path="/course", description="Operations about Course")
 
 @course_ns.route("")
+@course_ns.doc(responses={200: "OK", 201: "Created", 400: "Bad Request"})
 class CourseList(Resource):
-  @course_ns.marshal_list_with(course_response_model)
   def get(self):
     """Get a list of all courses."""
     courses = Course.query.all()
-    return courses
+    return generate_response(200, "Request processed successful", api.marshal(courses, course_response_model)), 200
 
   @course_ns.expect(course_model)
-  @course_ns.marshal_with(course_response_model, code=201)
   def post(self):
     """Add a new course."""
     # check if the name is already in use
     existing_course_name = Course.query.filter_by(name=api.payload["name"]).first()
     if existing_course_name:
-      api.abort(400, "Name is already in use")
+      return generate_response(400, "Name is already in use"), 400
     new_course = Course(name=api.payload["name"])
 
     # query for the students with the specified IDs
@@ -34,31 +34,29 @@ class CourseList(Resource):
 
     db.session.add(new_course)
     db.session.commit()
-    return new_course, 201
+    return generate_response(201, "Course created successful", api.marshal(new_course, course_response_model)), 201
 
 @course_ns.route("/<int:id>")
-@course_ns.doc(responses={404: "Course not found"}, params={"id": "Course ID"})
+@course_ns.doc(responses={200: "OK", 204: "No Content", 400: "Bad Request", 404: "Not Found"}, params={"id": "Course ID"})
 class CourseResource(Resource):
-  @course_ns.marshal_with(course_response_model)
   def get(self, id):
-    """Get course details by ID."""
+    """Get course by ID."""
     course = Course.query.get(id)
     if not course:
-      api.abort(404, "Course with ID {} not found".format(id))
-    return course
+      return generate_response(404, "Course not found"), 404
+    return generate_response(200, "Request processed successful", api.marshal(course, course_response_model)), 200
 
   @course_ns.expect(course_model)
-  @course_ns.marshal_with(course_response_model)
   def put(self, id):
     """Update courses by ID."""
     course = Course.query.get(id)
     if not course:
-      api.abort(404, "Course with ID {} not found".format(id))
+      return generate_response(404, "Course not found"), 404
 
     # check if the new name is already in use
     existing_course_name = Course.query.filter_by(name=api.payload["name"]).first()
     if existing_course_name and existing_course_name.id != id:
-      api.abort(400, "Name is already in use")
+      return generate_response(400, "Name is already in use"), 400
     course.name = api.payload["name"]
 
     # update associated students
@@ -66,13 +64,14 @@ class CourseResource(Resource):
       course.students = Student.query.filter(Student.id.in_(api.payload["student_ids"])).all()
 
     db.session.commit()
-    return course
+    return generate_response(200, "Course updated successful", api.marshal(course, course_response_model)), 200
 
   def delete(self, id):
     """Delete courses by ID."""
     course = Course.query.get(id)
     if not course:
-      api.abort(404, "Course with ID {} not found".format(id))
+      return generate_response(404, "Course not found"), 404
+
     db.session.delete(course)
     db.session.commit()
-    return "", 204
+    return generate_response(204, "Course deleted successful", api.marshal(course, course_response_model)), 204
